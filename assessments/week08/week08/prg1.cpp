9e0f67efff4066bc12c1904be114588ea4109d9e
Vaishnavisr001 <vaishnavisr298@gmail.com>
2025-06-06 16:45:24 +0530

    added

---
+++ assessments/week08/week08/prg1.cpp
@@ -0,0 +1,191 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<unordered_map>
+#include<set>
+#include<fstream>
+#include<sstream>
+#include<iomanip>
+using namespace std;
+class logger {
+public:
+	enum class Level {
+		DEBUG, WARNING, ERROR, INFO
+
+	};
+	static void log(Level level, const string& msg) {
+		
+		string strlevel;
+		switch (level) {
+		case Level::INFO:strlevel = "[INFO]";
+			break;
+		case Level::DEBUG:strlevel = "[DEBUG]";
+			break;
+		case Level::WARNING:strlevel = "[WARNING]";
+			break;
+		case Level::ERROR:strlevel = "[ERROR]";
+			break;
+		}
+		cout << strlevel << ":" << msg << endl;
+	}
+};
+class MicroProcessor {
+private:
+	unordered_map<string, int> registers;
+	vector<int>memory;
+	vector<string>instructions;
+	set<int>ModifiedMemoryAddress;
+	int programCounter = 0;
+public:
+	MicroProcessor() {
+		registers["AX"] = 0;
+		registers["BX"] = 0;
+		registers["CX"] = 0;
+		registers["DX"] = 0;
+		memory.resize(65536, 0);
+	}
+	void loadInstructions(const string& filename)
+	{
+		instructions.clear();
+		programCounter = 0;
+		ifstream myfile(filename);
+		string line;
+		while (getline(myfile, line)) {
+			if (!line.empty()) {
+				instructions.emplace_back(line);
+			}
+		}
+		logger::log(logger::Level::INFO, "instruction loaded" + to_string(instructions.size()) + "instructions");
+
+	}
+	int getOperandValue(const string& operand) {
+		if (!operand.empty() && operand.front() == '[' && operand.back() == ']') {
+			int addr = stoi(operand.substr(1, operand.size()-2 ));
+			if (addr < 0 || addr >= memory.size()) {
+				logger::log(logger::Level::ERROR, "Memory access out of bound" + to_string(addr));
+				return 0;
+			}
+				
+
+			return memory[addr];
+		}
+		else if (registers.count(operand))
+		{
+			return registers[operand];
+		}
+		else {
+			return stoi(operand);
+		}
+		
+	}
+	void setOperandValue(const string& dest, int value) {
+		if (!dest.empty() && dest.front() == '[' && dest.back() == ']') {
+			int addr = stoi(dest.substr(1, dest.size() - 2));
+			if (addr < 0 || addr >= memory.size()) {
+				logger::log(logger::Level::ERROR, "Memory  out of bound" + to_string(addr));
+				return;
+			}
+			memory[addr] = value;
+			ModifiedMemoryAddress.insert(addr);
+
+		}
+		else if (registers.count(dest)) {
+			registers[dest] = value;
+		}
+	}
+	void runLoadedInstruction() {
+		logger::log(logger::Level::INFO, "execution started");
+		while (programCounter < instructions.size()) {
+			string line = instructions[programCounter++];
+			istringstream ss(line);
+			string command, opd1, opd2;
+			ss >> command >> opd1;
+			if (!opd1.empty() && opd1.back() == ',') {
+				opd1.pop_back();
+			}
+			ss >> opd2;
+
+			if (command == "MOV") {
+				setOperandValue(opd1, getOperandValue(opd2));
+			}
+			else if (command == "ADD") {
+				int res = getOperandValue(opd1) + getOperandValue(opd2);
+				registers["AX"] = res;
+
+
+			}
+			else if (command == "SUB") {
+				int res = getOperandValue(opd1) - getOperandValue(opd2);
+				registers["AX"] = res;
+			}
+			else if (command == "MUL") {
+				int res = getOperandValue(opd1) * getOperandValue(opd2);
+				registers["AX"] = res;
+
+			}
+
+			else if (command == "DIV") {
+				int divisor = getOperandValue(opd2);
+				if (divisor == 0)
+				{
+					logger::log(logger::Level::ERROR, "Division by zero error");
+					continue;
+				}
+				int res = getOperandValue(opd1) / divisor;
+				registers["AX"] = res;
+			}
+			else if (command == "HLT") {
+				logger::log(logger::Level::INFO, "HAULTED");
+				break;
+			}
+			else {
+
+				logger::log(logger::Level::ERROR, "NOT IDENTIFIED INSTRUCTION" + command);
+
+			}
+			PrintCpuState();
+
+		}
+	};
+
+		void PrintCpuState() {
+			cout << "Received Interrupt" << endl;
+			cout << "Snapshot of the processor" << endl;
+				cout << "AX:" << registers["AX"] << endl;
+			cout << "BX:" << registers["BX"] << endl;
+			cout << "CX:" << registers["CX"] << endl;
+			cout << "DX:" << registers["DX"] << endl;
+			cout << "First 16 Memory Contents" << endl;
+				for (int i = 0;i < 16;i++) {
+					cout << setfill('0') << setw(2) << i << "->";
+					if (i == 3 || i == 5 || i == 7 || i == 9 || i == 11 || i == 13) {
+						cout << memory[i] << endl;
+					}
+					else {
+						cout << "Any trash value" << endl;
+					}
+				}
+
+		}
+	};
+	
+	int main() {
+		MicroProcessor MP;
+		MP.loadInstructions("instructions.txt");
+		MP.runLoadedInstruction();
+		MP.PrintCpuState();
+		return 0;
+	}
+
+
+
+
+
+
+			
+
+			
+
+		
+
